9b2b58fa9b11ecdb30cf95871c26477fee1115ba
Hunter13-cmr <felocksadrack@gmail.com>
2023-01-10 08:29:24 -0800

    Second task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,23 +7,23 @@
 **/
 char *_strdup(char *str)
 {
-char *strDup;
-int i, j;
+	char *strDup;
+	int i, j;
 
-if (str == NULL)
-	return (NULL);
-i = 0;
-while (str[i] != '\0')
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
 	i++;
-i++;
-strDup = malloc(sizeof(*str) * i);
-if (strDup == NULL)
-	return (NULL);
-j = 0;
-while (str[j] != '\0')
-	{
-		strDup[j] = str[j];
-		j++;
-	}
-	return (strDup);
+	strDup = malloc(sizeof(*str) * i);
+	if (strDup == NULL)
+		return (NULL);
+	j = 0;
+	while (str[j] != '\0')
+		{
+			strDup[j] = str[j];
+			j++;
+		}
+		return (strDup);
 }
