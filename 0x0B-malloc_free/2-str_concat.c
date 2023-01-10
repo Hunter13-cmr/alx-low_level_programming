46bcf203d8369324ced7a60115e7679b6a1b7db3
Hunter13-cmr <felocksadrack@gmail.com>
2023-01-10 12:24:22 -0800

    Third task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,35 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* str_concat - function that concatenates two strings
-* @s1: paramater for string 1
-* @s2: parameter for string 2
-* Return: Always 0.
-*/
+* str_concat - concatenates two strings
+* @s1: first string
+* @s2: second string
+* Return: pointer to new space in memory or null
+**/
 char *str_concat(char *s1, char *s2)
 {
-	char *concat_str;
-	int index, concat_index = 0, len = 0;
-
+	char *strDup;
+	int i, j;
 	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-
-	if (s == NULL)
-		s2 = "";
-
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-
-	concat_str = malloc(sizeof(char) * len);
-
-	if (concat_str == NULL)
+		i = j = 0;
+		while (s1[i] != '\0')
+			i++;
+		while (s2[j] != '\0')
+			j++;
+	strDup = malloc(sizeof(char) * (i + j + 1));
+	if (strDup == NULL)
 		return (NULL);
-
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
-
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-
-	return (concatstr);
+	i = j = 0;
+	while (s1[i] != '\0')
+		{
+			strDup[i] = s1[i];
+			i++;
+		}
+	while (s2[j] != '\0')
+		{
+			strDup[i] = s2[j];
+			i++, j++;
+		}
+	strDup[i] = '\0';
+		return (strDup);
 }
